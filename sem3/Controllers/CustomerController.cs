6d3dd3aed31759c457b0aa78de1f949a65eba36f
Thu Phuong <lephuong10193@gmail.com>
2023-07-13 21:41:41 +0700

    First Version

---
+++ sem3/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sem3.Models;
+using System.Data.SqlClient;
+
+namespace sem3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet("all")]
+        public async Task<IEnumerable<Customer>> GetCustomers()
+        {
+            var customers = new List<Customer>();
+            string _connectionString = "Server=mydb.database.windows.net;Database=OnlineCatere;User Id=Group4Catere;Password=@Hieu2104;";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                var commandText = "SELECT * FROM Customer;";
+
+                using (var command = new SqlCommand(commandText, connection))
+                {
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                            var customer = new Customer
+                            {
+                                CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                MiddleName = reader.GetString(reader.GetOrdinal("MiddleName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                Gender = reader.GetString(reader.GetOrdinal("Gender")),
+                                PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                TypeCustomer = reader.GetString(reader.GetOrdinal("TypeCustomer")),
+                                CLoginName = reader.GetString(reader.GetOrdinal("CLoginName")),
+                                Password = reader.GetString(reader.GetOrdinal("Password")),
+                                Flag = reader.GetBoolean(reader.GetOrdinal("Flag"))
+                            };
+                            customers.Add(customer);
+                        }
+                    }
+
+                }
+            }
+            return customers;
+        }
+    }
+}
---
+++ sem3/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace sem3.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? TypeCustomer { get; set; }
+        public string? CLoginName { get; set; }
+        public string? Password { get; set; }
+        public bool Flag { get; set; }
+    }
+}
