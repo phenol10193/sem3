865145bae62ae48d26f781d1341c4a0c7968decb
Thu Phuong <lephuong10193@gmail.com>
2023-07-19 23:04:42 +0700

    bo sung models

---
+++ sem3/Models/SuppDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace sem3.Models
+{
+    public class SuppDetail
+    {
+        public int SuppDetailId { get; set; }
+        public int SupplierId { get; set; }
+        public string? NameDetail { get; set; }
+        public int NumPeople { get; set; }
+        public float CustomerCost { get; set; }
+        public float SupplierCost { get; set; }
+        public bool Flag { get; set; }
+    }
+}
---
+++ sem3/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿namespace sem3.Models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string? SName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? SLevel { get; set; }
+        public int ACityId { get; set; }
+        public bool Flag { get; set; }
+    }
+}
